760441c08187e4fee37a4b75120516f2cb78c16d
server <fengxuting@qq.com>
2016-08-08 14:35:11 +0800

    Add biau tongji

--- header.php
+++ header.php
@@ -25,4 +25,14 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?0717308c48fcf14f31ec6a842c956e67";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
 </head>
